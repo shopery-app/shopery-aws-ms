d915dca3e782056ffb4eff6ce71b5f0d54c392db
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-05-25 20:23:51 +0400

    feature: add http methods

--- src/main/java/az/shopery/aws_ms/AwsMsApplication.java
+++ src/main/java/az/shopery/aws_ms/AwsMsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsMsApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(AwsMsApplication.class, args);
-	}
 
+	}
 }
