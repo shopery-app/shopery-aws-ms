8e14ed227e660b0bdd25ba791fde006c35021ecf
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-05-25 19:29:32 +0400

    project setup

---
+++ src/main/java/az/shopery/aws_ms/AwsMsApplication.java
@@ -0,0 +1,13 @@
+package az.shopery.aws_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/shopery/aws_ms/AwsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.shopery.aws_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
